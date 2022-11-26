fbd1df860635fb3a8ab81a34994930cd0ee7a71c
Daniel Walker <nickeldan@gmail.com>
2022-11-26 17:14:30 -0500

    incorporated test_failures.c into test_basic.c

--- tests/test_basic.c
+++ tests/test_basic.c
@@ -6,12 +6,6 @@ static SCR_TEST_FN(do_nothing)
     (void)_test_ctx;
 }
 
-static SCR_TEST_FN(skip_me)
-{
-    (void)_test_ctx;
-    SCR_TEST_SKIP();
-}
-
 static SCR_TEST_FN(integers_equal)
 {
     int x = 5, y = 5;
@@ -160,10 +154,158 @@ static SCR_TEST_FN(pointers_not_equal)
     SCR_ASSERT_PTR_NEQ(p1, p2);
 }
 
+static SCR_TEST_FN(fail_integers_equal)
+{
+    int x = 5, y = 6;
+
+    SCR_ASSERT_EQ(x, y);
+}
+
+static SCR_TEST_FN(fail_integers_not_equal)
+{
+    int x = 5, y = 5;
+
+    SCR_ASSERT_NEQ(x, y);
+}
+
+static SCR_TEST_FN(fail_integers_less_than)
+{
+    int x = 5, y = 5;
+
+    SCR_ASSERT_LT(x, y);
+}
+
+static SCR_TEST_FN(fail_integers_less_than_or_equal)
+{
+    int x = 5, y = 4;
+
+    SCR_ASSERT_LE(x, y);
+}
+
+static SCR_TEST_FN(fail_integers_greater_than)
+{
+    int x = 5, y = 5;
+
+    SCR_ASSERT_GT(x, y);
+}
+
+static SCR_TEST_FN(fail_integers_greater_than_or_equal)
+{
+    int x = 5, y = 6;
+
+    SCR_ASSERT_GE(x, y);
+}
+
+static SCR_TEST_FN(fail_unsigned_integers_equal)
+{
+    unsigned int x = 5, y = 6;
+
+    SCR_ASSERT_UNSIGNED_EQ(x, y);
+}
+
+static SCR_TEST_FN(fail_unsigned_integers_not_equal)
+{
+    unsigned int x = 5, y = 5;
+
+    SCR_ASSERT_UNSIGNED_NEQ(x, y);
+}
+
+static SCR_TEST_FN(fail_unsigned_integers_less_than)
+{
+    unsigned int x = 5, y = 5;
+
+    SCR_ASSERT_UNSIGNED_LT(x, y);
+}
+
+static SCR_TEST_FN(fail_unsigned_integers_less_than_or_equal)
+{
+    unsigned int x = 5, y = 4;
+
+    SCR_ASSERT_UNSIGNED_LE(x, y);
+}
+
+static SCR_TEST_FN(fail_unsigned_integers_greater_than)
+{
+    unsigned int x = 5, y = 5;
+
+    SCR_ASSERT_UNSIGNED_GT(x, y);
+}
+
+static SCR_TEST_FN(fail_unsigned_integers_greater_than_or_equal)
+{
+    unsigned int x = 5, y = 6;
+
+    SCR_ASSERT_UNSIGNED_GE(x, y);
+}
+
+static SCR_TEST_FN(fail_floats_equal)
+{
+    float x = 5.0, y = 6.0;
+
+    SCR_ASSERT_FLOAT_EQ(x, y);
+}
+
+static SCR_TEST_FN(fail_floats_not_equal)
+{
+    float x = 5.0, y = 5.0;
+
+    SCR_ASSERT_FLOAT_NEQ(x, y);
+}
+
+static SCR_TEST_FN(fail_floats_less_than)
+{
+    float x = 5.0, y = 5.0;
+
+    SCR_ASSERT_FLOAT_LT(x, y);
+}
+
+static SCR_TEST_FN(fail_floats_less_than_or_equal)
+{
+    float x = 6.0, y = 5.0;
+
+    SCR_ASSERT_FLOAT_LE(x, y);
+}
+
+static SCR_TEST_FN(fail_floats_greater_than)
+{
+    float x = 5.0, y = 5.0;
+
+    SCR_ASSERT_FLOAT_GT(x, y);
+}
+
+static SCR_TEST_FN(fail_floats_greater_than_or_equal)
+{
+    float x = 5.0, y = 6.0;
+
+    SCR_ASSERT_FLOAT_GE(x, y);
+}
+
+static SCR_TEST_FN(fail_pointers_equal)
+{
+    int x = 0, y = 0;
+    int *p1 = &x, *p2 = &y;
+
+    SCR_ASSERT_PTR_EQ(p1, p2);
+}
+
+static SCR_TEST_FN(fail_pointers_not_equal)
+{
+    int x = 0;
+    int *p1 = &x, *p2 = &x;
+
+    SCR_ASSERT_PTR_NEQ(p1, p2);
+}
+
+static SCR_TEST_FN(skip_me)
+{
+    (void)_test_ctx;
+    SCR_TEST_SKIP();
+}
+
 int
 main()
 {
-    unsigned int num_tests = 0;
+    unsigned int num_pass = 0, num_fail = 0, num_skip = 0;
     scrRunner *runner;
     scrGroup *group;
     scrStats stats;
@@ -171,38 +313,72 @@ main()
     runner = scrRunnerCreate();
     group = scrGroupCreate(runner, NULL, NULL);
 
-#define ADD_TEST(test)                          \
+#define ADD_PASS(test)                          \
     do {                                        \
         scrGroupAddTest(group, #test, test, 0); \
-        num_tests++;                            \
+        num_pass++;                             \
     } while (0)
-    ADD_TEST(do_nothing);
-    ADD_TEST(skip_me);
-    ADD_TEST(integers_equal);
-    ADD_TEST(integers_not_equal);
-    ADD_TEST(integers_less_than);
-    ADD_TEST(integers_less_than_or_equal);
-    ADD_TEST(integers_greater_than);
-    ADD_TEST(integers_greater_than_or_equal);
-    ADD_TEST(unsigned_integers_equal);
-    ADD_TEST(unsigned_integers_not_equal);
-    ADD_TEST(unsigned_integers_less_than);
-    ADD_TEST(unsigned_integers_less_than_or_equal);
-    ADD_TEST(unsigned_integers_greater_than);
-    ADD_TEST(unsigned_integers_greater_than_or_equal);
-    ADD_TEST(floats_equal);
-    ADD_TEST(floats_not_equal);
-    ADD_TEST(floats_less_than);
-    ADD_TEST(floats_less_than_or_equal);
-    ADD_TEST(floats_greater_than);
-    ADD_TEST(floats_greater_than_or_equal);
-    ADD_TEST(pointers_equal);
-    ADD_TEST(pointers_not_equal);
-#undef ADD_TEST
+#define ADD_FAIL(test)                                         \
+    do {                                                       \
+        scrGroupAddTest(group, "fail_" #test, fail_##test, 0); \
+        num_fail++;                                            \
+    } while (0)
+#define ADD_SKIP(test)                          \
+    do {                                        \
+        scrGroupAddTest(group, #test, test, 0); \
+        num_skip++;                             \
+    } while (0)
+
+    ADD_PASS(do_nothing);
+    ADD_PASS(integers_equal);
+    ADD_PASS(integers_not_equal);
+    ADD_PASS(integers_less_than);
+    ADD_PASS(integers_less_than_or_equal);
+    ADD_PASS(integers_greater_than);
+    ADD_PASS(integers_greater_than_or_equal);
+    ADD_PASS(unsigned_integers_equal);
+    ADD_PASS(unsigned_integers_not_equal);
+    ADD_PASS(unsigned_integers_less_than);
+    ADD_PASS(unsigned_integers_less_than_or_equal);
+    ADD_PASS(unsigned_integers_greater_than);
+    ADD_PASS(unsigned_integers_greater_than_or_equal);
+    ADD_PASS(floats_equal);
+    ADD_PASS(floats_not_equal);
+    ADD_PASS(floats_less_than);
+    ADD_PASS(floats_less_than_or_equal);
+    ADD_PASS(floats_greater_than);
+    ADD_PASS(floats_greater_than_or_equal);
+    ADD_PASS(pointers_equal);
+    ADD_PASS(pointers_not_equal);
+    ADD_FAIL(integers_equal);
+    ADD_FAIL(integers_not_equal);
+    ADD_FAIL(integers_less_than);
+    ADD_FAIL(integers_less_than_or_equal);
+    ADD_FAIL(integers_greater_than);
+    ADD_FAIL(integers_greater_than_or_equal);
+    ADD_FAIL(unsigned_integers_equal);
+    ADD_FAIL(unsigned_integers_not_equal);
+    ADD_FAIL(unsigned_integers_less_than);
+    ADD_FAIL(unsigned_integers_less_than_or_equal);
+    ADD_FAIL(unsigned_integers_greater_than);
+    ADD_FAIL(unsigned_integers_greater_than_or_equal);
+    ADD_FAIL(floats_equal);
+    ADD_FAIL(floats_not_equal);
+    ADD_FAIL(floats_less_than);
+    ADD_FAIL(floats_less_than_or_equal);
+    ADD_FAIL(floats_greater_than);
+    ADD_FAIL(floats_greater_than_or_equal);
+    ADD_FAIL(pointers_equal);
+    ADD_FAIL(pointers_not_equal);
+    ADD_SKIP(skip_me);
+
+#undef ADD_PASS
+#undef ADD_FAIL
+#undef ADD_SKIP
 
     scrRunnerRun(runner, NULL, &stats);
     scrRunnerDestroy(runner);
 
-    return (stats.num_passed != num_tests - 1 || stats.num_skipped != 1 || stats.num_failed != 0 ||
+    return (stats.num_passed != num_pass || stats.num_skipped != num_skip || stats.num_failed != num_fail ||
             stats.num_errored != 0);
 }
